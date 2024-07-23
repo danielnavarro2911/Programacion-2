844d08a8f015ed18aa1d09a19179a56b4bf5de01
Daniel Navarro Quiros <96166574+danielnavarro2911@users.noreply.github.com>
2024-07-22 22:49:06 -0600

    Add files via upload

---
+++ examen2_DN/examen2_DN/vistas/inicio.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/vistas/menu.Master" AutoEventWireup="true" CodeBehind="inicio.aspx.cs" Inherits="examen2_DN.vistas.inicio" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Inicio</h1>
+</asp:Content>
---
+++ examen2_DN/examen2_DN/vistas/resultados.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace examen2_DN.vistas
+{
+    public partial class resultados : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LlenarGrid();
+
+        }
+        protected void LlenarGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT *  FROM VotosPorPartido2"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    
+}
